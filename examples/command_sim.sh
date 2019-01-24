7cba46861494eeeb7bef92702fb2f270c39cced4
w6688j <953372680@qq.com>
2019-01-24 11:18:13 +0800

    edit .sh

--- examples/command_sim.sh
+++ examples/command_sim.sh
@@ -5,7 +5,7 @@ python PDTB_Bert.py \
   --do_eval \
   --do_lower_case \
   --data_dir ../data/ \
-  --bert_model bert-base-uncased \
+  --bert_model ../uncased_L-12_H-768_A-12/ \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
