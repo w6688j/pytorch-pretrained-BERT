c3c43c2fb4bb3fe68cd04d3a2acecdbb3eb766bc
w6688j <953372680@qq.com>
2019-01-24 10:37:00 +0800

    add MyModel

---
+++ command_sim.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+python example/PDTB_Bert.py \
+  --task_name sim \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir=data \
+  --bert_model bert-base-uncased \
+  --max_seq_length 128 \
+  --train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir=models/sim_model/
